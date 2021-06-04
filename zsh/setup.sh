b25195e6b4f270c37309ca5ea63fa7c249f37e8d
Lawrence Ma <lawrencema92@gmail.com>
2021-06-04 13:55:00 -0700

    add git aliases

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -11,3 +11,37 @@ if command -v curl >/dev/null 2>&1; then
 else
   sh -c "$(wget -O- https://raw.githubusercontent.com/romkatv/zsh4humans/v5/install)"
 fi
+
+# git aliases
+alias ga='git add'
+alias gaa='git add --all'
+alias gapa='git add --patch'
+alias gau='git add --update'
+
+# Check if main exists and use instead of master
+function git_main_branch() {
+  command git rev-parse --git-dir &>/dev/null || return
+  local branch
+  for branch in main trunk; do
+    if command git show-ref -q --verify refs/heads/$branch; then
+      echo $branch
+      return
+    fi
+  done
+  echo master
+}
+
+alias gb='git branch'
+alias gcm='git checkout $(git_main_branch)'
+alias gco='git checkout'
+alias gd='git diff'
+
+alias gpsup='git push --set-upstream origin $(git symbolic-ref --short HEAD)'
+
+alias gl='git pull'
+
+alias glog='git log --oneline --decorate --graph'
+
+alias gp='git push'
+
+alias gst='git status'
